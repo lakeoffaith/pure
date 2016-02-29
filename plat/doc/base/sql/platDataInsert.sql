c2f3a7a6e58103af0d4fef7a8fcfb469f687ad74
lakeoffaith <905663280@qq.com>
2016-02-29 22:28:30 +0800

    first file

--- plat/doc/base/sql/platDataInsert.sql
+++ plat/doc/base/sql/platDataInsert.sql
@@ -27,83 +27,83 @@ values(2,'用户2的登陆信息');
 
 insert into
 ROLE(id,name,describe)
-values('1','管理员','系统管理员');
+values(ROLE_SEQ.nextval,'管理员','系统管理员');
 
 
 insert into
 ROLE(id,name,describe)
-values('2','用户','普通用户');
+values(ROLE_SEQ.nextval,'用户','普通用户');
 update EMPLOYEE set role_id=1 where id=1;
 update EMPLOYEE set role_id=2 where id=2;
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('1','管理系统',NULL,NULL,1,1,'icon-cogs');
+values(MENU_SEQ.NEXTVAL,'管理系统',NULL,NULL,1,1,'icon-cogs');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('2','应用管理',NULL,NULL,2,1,'icon-tablet');
+values(MENU_SEQ.NEXTVAL,'应用管理',NULL,NULL,2,1,'icon-tablet');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('3','个人办公',NULL,NULL,3,1,'icon-user');
+values(MENU_SEQ.NEXTVAL,'个人办公',NULL,NULL,3,1,'icon-user');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('4','项目管理','task',NULL,4,1,'icon-paste');
+values(MENU_SEQ.NEXTVAL,'项目管理','task',NULL,4,1,'icon-paste');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('5','统计分析','application',NULL,5,1,'icon-bar-chart');
+values(MENU_SEQ.NEXTVAL,'统计分析','application',NULL,5,1,'icon-bar-chart');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('6','日志管理','sysloginfo',NULL,6,1,'icon-book');
+values(MENU_SEQ.NEXTVAL,'日志管理','sysloginfo',NULL,6,1,'icon-book');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('7','在线咨询',NULL,NULL,7,1,'icon-comment');
+values(MENU_SEQ.NEXTVAL,'在线咨询',NULL,NULL,7,1,'icon-comment');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('8','角色管理','role',1,1,1,'');
+values(MENU_SEQ.NEXTVAL,'角色管理','role',1,1,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('9','员工管理','employee',1,2,1,'');
+values(MENU_SEQ.NEXTVAL,'员工管理','employee',1,2,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('10','菜单管理','menu',1,3,1,'');
+values(MENU_SEQ.NEXTVAL,'菜单管理','menu',1,3,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('11','权限配置','permission',1,4,1,'');
+values(MENU_SEQ.NEXTVAL,'权限配置','permission',1,4,1,'');
 
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('12','应用配置','application',2,1,1,'');
+values(MENU_SEQ.NEXTVAL,'应用配置','application',2,1,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('13','应用查询','application',2,2,1,'');
+values(MENU_SEQ.NEXTVAL,'应用查询','application',2,2,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('14','我的应用','application',3,1,1,'');
+values(MENU_SEQ.NEXTVAL,'我的应用','application',3,1,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('15','我的任务','task',3,2,1,'');
+values(MENU_SEQ.NEXTVAL,'我的任务','task',3,2,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('16','用户反馈','feedback',7,1,1,'');
+values(MENU_SEQ.NEXTVAL,'用户反馈','feedback',7,1,1,'');
 
 insert into
 MENU(id,name,url,parent_id,orderid,type,iconname)
-values('17','反馈管理','feedback',7,2,1,'');
+values(MENU_SEQ.NEXTVAL,'反馈管理','feedback',7,2,1,'');
 
 insert into
 ROLE_MENU(role_id,menu_id)
@@ -132,15 +132,15 @@ values(2,2);
 
 insert into 
 APPLICATION(id,name,brief,state,docpath)
-values(1,'预约挂号'，'预约挂号'，1，'预约中心');
+values(APPLICATION_SEQ.NEXTVAL,'预约挂号'，'预约挂号'，1，'预约中心');
 
 insert into
 APPLICATION(id,name,brief,state,docpath)
-values(2,'院长决策'，'院长决策'，1，'院长办公室');
+values(APPLICATION_SEQ.NEXTVAL,'院长决策'，'院长决策'，1，'院长办公室');
 
 insert into
 FEEDBACK(id,content)
-values(1,'系统不错');
+values(FEEDBACK_SEQ.NEXTVAL,'系统不错');
 
 
 insert into
